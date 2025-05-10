8af05fac7513610844b4b1cc63b724fdf7261b84
Anastasia Prodan <prodananastasija@yandex.com>
2025-05-10 22:48:52 +0300

    add_test_constructor_section

---
+++ tests/test_constructor_section.py
@@ -0,0 +1,28 @@
+from data import BASE_URL
+from helpers import wait_for_element
+import locators as loc
+
+
+def test_buns_section(driver):
+    driver.get(BASE_URL)
+    wait_for_element(driver, *loc.sauces_block).click()
+    wait_for_element(driver, *loc.buns_block).click()
+    selected = wait_for_element(driver, *loc.selected_button)
+    assert "Булки" in selected.text
+    driver.quit()
+
+
+def test_sauces_section(driver):
+    driver.get(BASE_URL)
+    wait_for_element(driver, *loc.sauces_block).click()
+    selected = wait_for_element(driver, *loc.selected_button)
+    assert "Соусы" in selected.text
+    driver.quit()
+
+
+def test_fillings_section(driver):
+    driver.get(BASE_URL)
+    wait_for_element(driver, *loc.fillings_block).click()
+    selected = wait_for_element(driver, *loc.selected_button)
+    assert "Начинки" in selected.text
+    driver.quit()
