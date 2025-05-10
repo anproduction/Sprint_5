b9b9f20127225d73d6944292a1757a3424ce54f9
Anastasia Prodan <prodananastasija@yandex.com>
2025-05-10 22:37:14 +0300

    add_data_file

---
+++ data.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+BASE_URL = "https://stellarburgers.nomoreparties.site/"
+
+FIRST_NAME = "Анастасия"
+LAST_NAME = "Продан"
+COHORT_NUMBER = "22"
+DOMAIN = "yandex.ru"
+
+def generate_random_string(length=8):
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
+
+def generate_unique_email():
+    random_digits = ''.join(random.choices(string.digits, k=3))
+    email = f"{FIRST_NAME.lower()}{LAST_NAME.lower()}{COHORT_NUMBER}{random_digits}@{DOMAIN}"
+    return email
+
+def generate_random_password():
+    return generate_random_string(length=8)
+
+TEST_USER = {
+    "name": FIRST_NAME,
+    "email": generate_unique_email(),
+    "password": generate_random_password()
+}
+
+INVALID_USER = {
+    "email": "invalid_email@yandex.ru",
+    "password": "short"
+}
