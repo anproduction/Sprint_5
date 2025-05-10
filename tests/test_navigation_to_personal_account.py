d66fa9493aaf0ec99e76f289a197708005e3ed90
Anastasia Prodan <prodananastasija@yandex.com>
2025-05-10 22:45:52 +0300

    add_test_navigation_to_personal_account

---
+++ tests/test_navigation_to_personal_account.py
@@ -0,0 +1,17 @@
+from data import TEST_USER, BASE_URL
+from helpers import wait_for_element
+import locators as loc
+
+
+def login(driver):
+    driver.get(BASE_URL + "/login")
+    wait_for_element(driver, *loc.input_email_auth).send_keys(TEST_USER["email"])
+    wait_for_element(driver, *loc.input_password_auth).send_keys(TEST_USER["password"])
+    wait_for_element(driver, *loc.button_login).click()
+
+
+def test_navigation_to_personal_account(driver):
+    login(driver)
+    wait_for_element(driver, *loc.button_personal_account).click()
+    wait_for_element(driver, *loc.profile)
+    driver.quit()
